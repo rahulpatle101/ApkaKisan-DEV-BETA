ed15e020d8c63df789f700370368f3b61be2c322
momersaleem <momersaleem@gmail.com>
2022-08-09 14:27:00 +0500

    Fixed issues.

--- app/src/main/java/com/apkakisan/myapplication/profile/EditProfileViewModel.kt
+++ app/src/main/java/com/apkakisan/myapplication/profile/EditProfileViewModel.kt
@@ -1,12 +1,10 @@
 package com.apkakisan.myapplication.profile
 
 import android.net.Uri
-import android.telephony.PhoneNumberUtils
 import androidx.lifecycle.viewModelScope
 import com.apkakisan.myapplication.BaseViewModel
 import com.apkakisan.myapplication.User
 import com.apkakisan.myapplication.helpers.LocalStore
-import com.apkakisan.myapplication.utils.BuildTypeUtil
 import com.apkakisan.myapplication.utils.PhoneNoUtil
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -19,7 +17,7 @@ class EditProfileViewModel(
     private var _uiState = MutableSharedFlow<EditProfileUiState>()
     val uiState: SharedFlow<EditProfileUiState> = _uiState
 
-    val user: User = LocalStore.getUser()!!
+    val user: User = LocalStore.getUser() ?: User()
 
     var photo = ""
     var name = ""
@@ -31,7 +29,7 @@ class EditProfileViewModel(
         name = user.fullName ?: ""
         user.phoneNumber?.let {
             phone = it.substring(3, it.length)
-            phone = PhoneNumberUtils.formatNumber(phone, "US")
+            phone = PhoneNoUtil.format10DigitToUS(phone)
         }
         address = user.location ?: ""
     }
@@ -41,14 +39,10 @@ class EditProfileViewModel(
     }
 
     fun validate() = viewModelScope.launch {
-        phone = phone.filter { it.isLetterOrDigit() || it.isWhitespace() }
-        phone = phone.replace(" ", "")
         when {
             name.isEmpty() -> _uiState.emit(EditProfileUiState.EMPTY_NAME)
-            phone.length < 10 -> {
-                _uiState.emit(EditProfileUiState.INVALID_PHONE)
-                if (phone.isEmpty()) _uiState.emit(EditProfileUiState.EMPTY_PHONE)
-            }
+            phone.isEmpty() -> _uiState.emit(EditProfileUiState.EMPTY_PHONE)
+            (phone.length < 10 || phone.length > 10) -> _uiState.emit(EditProfileUiState.INVALID_PHONE)
             address.isEmpty() -> _uiState.emit(EditProfileUiState.EMPTY_ADDRESS)
             else -> _uiState.emit(EditProfileUiState.DATA_VALIDATED)
         }
@@ -67,21 +61,23 @@ class EditProfileViewModel(
 
     fun updateUser() = viewModelScope.launch {
         _uiState.emit(EditProfileUiState.PROFILE_UPDATING)
-
-        if (BuildTypeUtil.isDebug() || BuildTypeUtil.isDebugWithRegistration())
-            phone = PhoneNumberUtils.formatNumberToE164(phone, "PK")
-        if (BuildTypeUtil.isRahul())
-            phone = PhoneNumberUtils.formatNumberToE164(phone, "IN")
-
+        phone = PhoneNoUtil.formatForServer(phone)
         val isUpdated = repository.updateUser(
             user.userId,
             name,
             phone,
             address
         )
-        if (isUpdated)
+        if (isUpdated) {
             _uiState.emit(EditProfileUiState.PROFILE_UPDATE_SUCCESS)
-        else
+            LocalStore.getUser()?.apply {
+                fullName = name
+                phoneNumber = phone
+                location = address
+            }?.also {
+                it.save()
+            }
+        } else
             _uiState.emit(EditProfileUiState.PROFILE_UPDATE_FAILED)
     }
 }
@@ -98,15 +94,4 @@ enum class EditProfileUiState {
     PROFILE_UPDATING,
     PROFILE_UPDATE_SUCCESS,
     PROFILE_UPDATE_FAILED
-//    object PhotoUploading : EditProfileUiState()
-//    object PhotoUploadSuccess : EditProfileUiState()
-//    object PhotoUploadFailed : EditProfileUiState()
-//    object EmptyName : EditProfileUiState()
-//    object InvalidPhone : EditProfileUiState()
-//    object EmptyPhone : EditProfileUiState()
-//    object EmptyAddress : EditProfileUiState()
-//    object DataValidated : EditProfileUiState()
-//    object ProfileUpdating : EditProfileUiState()
-//    object ProfileUpdateSuccess : EditProfileUiState()
-//    object ProfileUpdateFailed : EditProfileUiState()
 }
